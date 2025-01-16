241374ad9f33d6920d2f5f21774bf77463404886
Alexsander Valente <alevtelles@gmail.com>
2025-01-16 10:17:08 -0300

    create ui components

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+import Hero from "./components/landing-page/hero";
+
 export default function Home() {
-  return <div className="bg-background-primary">Hello word</div>;
+  return (
+    <div className=" border h-screen max-w-7xl mx-auto">
+      <Hero />
+      {/* <Header />
+     
+      <VideoExplanation />
+      <Price />
+      <Faq /> */}
+    </div>
+  );
 }
