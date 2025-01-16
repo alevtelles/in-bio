a527919fb93fe75ba81e3a57e5410917c5bc3513
Alexsander Valente <alevtelles@gmail.com>
2025-01-16 19:42:52 -0300

    add component total visits

--- app/components/commons/user-card.tsx
+++ app/components/commons/user-card.tsx
@@ -14,11 +14,13 @@ export default function UserCard() {
       </div>
       <div className="flex flex-col gap-2 w-full">
         <div className="flex items-center gap-2">
-          <h3 className="text-3xl font-bold min-w-0 overflow-hidden">
+          <h3 className="text-3xls font-bold min-w-0 overflow-hidden ">
             Alexsander Valente
           </h3>
         </div>
-        <p className="opacity-40">"A única constante é a mudança</p>
+        <p className="opacity-40 text-center">
+          "A única constante é a mudança"
+        </p>
       </div>
       <div className="flex flex-col gap-2 w-full">
         <span className="uppercase text-xs font-medium">Links</span>
@@ -32,14 +34,13 @@ export default function UserCard() {
             </button>
           ))}
         </div>
-
-        <div className="flex flex-col gap-3 w-full h-[172px]">
-          <div className="w-full flex flex-col items-center gap-3">
-            <Button className="w-full"> Template Sass - Compre Agora!</Button>
-            <button className="p-3 rounded-xl bg-[#1e1e1e] hover:bg-[#2e2e2e]">
-              <Plus />
-            </button>
-          </div>
+      </div>
+      <div className="flex flex-col gap-3 w-full h-[172px]">
+        <div className="w-full flex flex-col items-center gap-3">
+          <Button className="w-full"> Template Sass - Compre Agora!</Button>
+          <button className="p-3 rounded-xl bg-[#1e1e1e] hover:bg-[#2e2e2e]">
+            <Plus />
+          </button>
         </div>
       </div>
     </div>
--- app/components/landing-page/hero.tsx
+++ app/components/landing-page/hero.tsx
@@ -1,3 +1,4 @@
+import TotalVisits from "../commons/total-visits";
 import UserCard from "../commons/user-card";
 import Button from "../ui/button";
 import TextArea from "../ui/text-area";
@@ -26,7 +27,7 @@ export default function Hero() {
         <div className="relative">
           <UserCard />
           <div className="absolute -bottom-[7%] -right-[45%]">
-            {/* <TotalVisits /> */}
+            <TotalVisits />
           </div>
           <div className="absolute top-[20%] -left-[45%] -z-10">
             {/* <ProjectCard /> */}
