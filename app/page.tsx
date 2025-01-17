46054c9ceb79aebf0d0f7b76979fab7617af4097
Alexsander Valente <alevtelles@gmail.com>
2025-01-17 09:38:51 -0300

    add component video

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 import Header from "./components/landing-page/header";
 import Hero from "./components/landing-page/hero";
+import VideoExplanation from "./components/landing-page/video-explanation";
 
 export default function Home() {
   return (
     <div className="h-screen max-w-7xl mx-auto">
       <Header />
       <Hero />
-      {/* <Header />
-     
+
       <VideoExplanation />
-      <Price />
-      <Faq /> */}
+      {/* <Price />
+      <Faq />  */}
     </div>
   );
 }
