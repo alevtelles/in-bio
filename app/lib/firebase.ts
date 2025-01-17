b275e2c7bfbb265095aa4531a0a7a96916c25ca9
Alexsander Valente <alevtelles@gmail.com>
2025-01-17 18:02:03 -0300

    add manage auth and routes with firebase

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -3,27 +3,27 @@ import { getFirestore } from "firebase-admin/firestore";
 import { getStorage } from "firebase-admin/storage";
 import "server-only";
 
-// Certificado
+// Certifcado
+
 const decodedKey = Buffer.from(
-    process.env.FIREBASE_PRIVATE_KEY!, 
-    "base64"
+  process.env.FIREBASE_PRIVATE_KEY_BASE64!,
+  "base64"
 ).toString("utf-8");
 
-export const firebaseCert =cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: decodedKey,
+export const firebaseCert = cert({
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: decodedKey,
 });
 
-// Instancia do App
-if(!getApps().length){
-    initializeApp({
+// Instancia do app
+if (!getApps().length) {
+  initializeApp({
     credential: firebaseCert,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET
-});
-
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  });
 }
 
+export const db = getFirestore();
 
-export const db = getFirestore()
 export const storage = getStorage().bucket();
